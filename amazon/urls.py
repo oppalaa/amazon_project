387e9ebd1cdb2f2627f0dab671d1996257f13a16
oppalaa <oppala7@gmail.com>
2023-08-03 14:09:45 +0530

    first commit

---
+++ amazon/urls.py
@@ -0,0 +1,40 @@
+"""
+URL configuration for amazon project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from app.views import *
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('registration/',registration,name='registration'),
+    path('home/',home,name='home'),
+    path('user_login/',user_login,name='user_login'),
+    path('user_logout/',user_logout,name='user_logout'),
+    path('display_details/',display_details,name='display_details'),
+    path('change_password/',change_password,name='change_password'),
+    path('mobiles/',mobiles,name='mobiles'),
+    path('today_deals/',today_deals,name='today_deals'),
+    path('amazon_mini_tv/',amazon_mini_tv,name='amazon_mini_tv'),
+    path('Homee/',Homee,name='Homee'),
+    path('webseries/',webseries,name='webseries'),
+    path('short_films/',short_films,name='short_films'),
+    path('comedy/',comedy,name='comedy'),
+    path('Electronics/',Electronics,name='Electronics'),
+    path('reset_password/',reset_password,name='reset_password'),
+    
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
